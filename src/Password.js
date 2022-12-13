151aeafcffa665c1b3a3569207b27725dfe2dd7c
Shani Yonayov <shanipninayonayov1@gmail.com>
2022-12-13 09:14:50 +0200

    HW5-CheckPassword

---
+++ src/Password.js
@@ -0,0 +1,28 @@
+import { useState,useEffect } from "react";
+
+function Password(props) {
+  
+  const {input} = props;
+  const[Strange,setStrange] = useState("Short password");
+
+  useEffect(()=>{
+    if(input.length <=2){
+      setStrange("Short password")
+    }
+    else if(input.length <=6){
+      setStrange("Medium password")
+    }
+    else if(input.length >7){setStrange("Strong password")}
+  },[setStrange,input])
+
+
+
+  return (
+    <div>
+    <h3>{Strange}</h3>
+
+  
+    </div>
+  );
+}
+export default Password;
